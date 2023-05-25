b6b09b9b0f2d7f5adfa34ad52f37e49beef68666
Divyesh Vankar <divyeshvankar.iitg@gmail.com>
2023-05-25 19:53:17 +0530

    added mongoose with node

---
+++ backend/db.js
@@ -0,0 +1,17 @@
+const mongoose =require('mongoose');
+// const mongoURI ="mongodb://127.0.0.1:27017";
+
+
+
+const connectToMongo = async()=> {
+    mongoose.connect("mongodb://127.0.0.1:27017/iNotebook");
+    const db= mongoose.connection;
+    db.on("error",console.error.bind(console,"connection error: "));
+    db.once("open",function(){
+        console.log("connected successfully");
+    })  
+}
+
+
+
+module.exports =connectToMongo;
