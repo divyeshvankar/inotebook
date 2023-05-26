c217f49d1278fb27df5c81b0f9de410d4edf5748
Divyesh Vankar <divyeshvankar.iitg@gmail.com>
2023-05-26 13:07:40 +0530

    auth token added  and password security increse

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,8 +2,11 @@ const express =require ('express');
 const router = express.Router();
 const User = require ('../models/User');
 const {body , validationResult}=require ('express-validator');
+const bcrypt =require('bcryptjs');
+var jwt = require('jsonwebtoken');
 
 
+const JWR_SECRET = 'Divyesh_is_goodboy$'
 router.post('/',[
     // validation of users info
     body('name','enter valid name').isLength({min:3}),
@@ -26,12 +29,16 @@ let user= await User.findOne({email: req.body.email});;
 if(user){
     return res.status(400).json({error:"Sorry a user with this mail already exists "})
 }
+
+// salting and hashing of password
+const salt= await bcrypt.genSalt(10);
+const secPass=await bcrypt.hash(req.body.password,salt);
 // creating user 
 user = await User.create({
     name: req.body.name,
-    password: req.body.password,
+    password: secPass,
     email:req.body.email,
-})
+});
 
 // .then(user=>res.json(user))
 //   .catch(err=> {console.log(err)
@@ -39,7 +46,20 @@ user = await User.create({
 //     error: 'please eneter unique value of email',
 //     message: err.message
 //   })
-res.json(user);
+
+
+//  Following for authorization of user 
+const data={
+    user:{
+        id:user.id
+    }
+}
+// giving user authorisation token 
+const authToken=jwt.sign(data,JWR_SECRET);
+// console.log(jwtData);
+
+// res.json(user);
+res.json(authToken);
 } catch (error){
     console.log(error.message);
     res.status(500).send("Some error occures");
